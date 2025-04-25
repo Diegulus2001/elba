c848a8eb0018f6de94418e917f0780114eb5ed86
Diegulus2001 <007.diegov@gmail.com>
2025-04-25 15:22:04 +0200

    Translatable content and load different sections

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-$(document).on("touchstart", "#menu-toggle", function() {
+$(document).on("touchstart", "#menu-toggle", function () {
     $("#left-nav").toggleClass("active");
 });
 
-$(document).on("mousemove", function(e) {
+// * Custom Cursor
+$(document).on("mousemove", function (e) {
     $(".custom-cursor").css({
         left: e.clientX + "px",
         top: e.clientY + "px",
         transform: "translate(-50%, -50%) scale(1.2)"
-    }).delay(50).queue(function(next) {
+    }).delay(50).queue(function (next) {
         $(this).css("transform", "translate(-50%, -50%) scale(1)");
         next();
+    });
+});
+
+// * Translations
+$(document).ready(function () {
+    const currentLang = $("html").attr("lang");
+    $("[id$='-trans']").each(function () {
+        const id = $(this).attr("id");
+        $(this).text(translations[currentLang][id]);
+    });
+});
+
+$(document).on("click", "#lang-toggle", function () {
+    const currentLang = $("html").attr("lang");
+    const newLang = currentLang === "es" ? "en" : "es";
+    $("html").attr("lang", newLang);
+
+    // # IMPORTANT -> Translatable elements must have an id that ends with "-trans" AND their own translations in the translations.js file
+    $("[id$='-trans']").each(function () {
+        const id = $(this).attr("id");
+        $(this).text(translations[newLang][id]);
+    });
+
+    // Update lang flag
+    const flagSrc = newLang === "es" ? "images/gb.svg" : "images/es.svg";
+    $("#lang-toggle").attr("src", flagSrc);
+});
+
+// * Nav Menu
+$(document).ready(function () {
+    // Mostrar la sección correspondiente al enlace clickeado
+    $("nav a").click(function (e) {
+        e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+
+        // Obtener el valor del atributo data-target
+        var target = $(this).data("target");
+
+        // Ocultar todas las secciones
+        $(".content-section").hide();
+
+        // Mostrar la sección correspondiente
+        $("#" + target).show();
     });
 });
---
+++ translations.js
@@ -0,0 +1,20 @@
+const translations = {
+    es: {
+        "about-trans": "sobre mí",
+        "welcome-trans": "bienvenido a mi página",
+        "welcome-content-trans": "este es el contenido de la página 1.",
+        "projects-trans": "proyectos",
+        "projects-content-trans": "este es el contenido de la página 2.",
+        "contact-trans": "contacto",
+        "contact-content-trans": "este es el contenido de la página 3.",
+    },
+    en: {
+        "about-trans": "about me",
+        "welcome-trans": "welcome to my page",
+        "welcome-content-trans": "this is the content of the page 1.",
+        "projects-trans": "projects",
+        "projects-content-trans": "this is the content of the page 2.",
+        "contact-trans": "contact",
+        "contact-content-trans": "this is the content of the page 3.",
+    }
+};
